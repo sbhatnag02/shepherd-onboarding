f9835b8ec26ad4395fdb98aa7aa01e4ad313ba8a
Sarthak Bhatnagar <s.bhatnag02@gmail.com>
2021-10-23 13:20:43 -0700

    [MINI-PROJ-1] HTML/CSS done, need to add javascript feature

--- MINI-PROJ-1/student/views.py
+++ MINI-PROJ-1/student/views.py
@@ -5,11 +5,18 @@ app = Flask(__name__)
 
 # Refer to https://flask.palletsprojects.com/en/2.0.x/quickstart/
 
-@app.route('/')
-def index():
-    pass
-    # Your code here
+@app.route('/<page_name>')
+def index(page_name):
+    return render_template(page[page_name])
 
+@app.route('/page1')
+@app.route('/page1alt')
+def page1():
+    return "this is page 1"
+
+page = {
+    "intro": "your_code.html"
+}
 
 if __name__ == "__main__":
     app.run(debug=True)
